aa8ce10ccaaa93d71c1769c4cf7436955622969b
Mike <mikedelano075@gmail.com>
2025-10-11 22:20:21 +1100

    Added header comments to pose_trajectory_node and camera_node

--- turtlebot3_ws/src/turtlebot3_simulations/turtlebot3_gazebo/include/turtlebot3_gazebo/turtlebot3_camera.hpp
+++ turtlebot3_ws/src/turtlebot3_simulations/turtlebot3_gazebo/include/turtlebot3_gazebo/turtlebot3_camera.hpp
@@ -8,26 +8,36 @@
 #include <opencv2/opencv.hpp>
 #include <mutex> 
 
+/**
+ * @file turtlebot3_camera.hpp
+ * @brief Camera_Node for Turtlebot3
+ * 
+ * This class encapsulates the subscribers, publishers, and helper functions
+ * for Turtlebot to determine whether or not it has reached the goal state.
+ */
+
 class CameraNode : public rclcpp::Node
 {
     public:
         CameraNode();
 
+        
         bool get_latest_frames(cv::Mat &camera_frame, cv::Mat &mask_frame);
 
     private:
 
+        // Callback function for image subscription
         void image_callback(const sensor_msgs::msg::Image::SharedPtr p_msg);
 
-
+        // Checks to see if the goal state is reached and publishes if goal is reached
         void check_goal(const sensor_msgs::msg::Image::SharedPtr p_msg);
 
 
         // ROS publishers and subscribers
         rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr m_stop_pub;
         rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr m_image_sub;
-        //rclcpp::Publisher<std_msgs::msg::bool>::SharedPtr m_dash_pub;
 
+        // Mutex and Mat objects for handling threading
         std::mutex image_mutex_;
         cv::Mat latest_camera_frame_;
         cv::Mat latest_mask_frame_;
--- turtlebot3_ws/src/turtlebot3_simulations/turtlebot3_gazebo/include/turtlebot3_gazebo/turtlebot3_pose_trajectory.hpp
+++ turtlebot3_ws/src/turtlebot3_simulations/turtlebot3_gazebo/include/turtlebot3_gazebo/turtlebot3_pose_trajectory.hpp
@@ -7,17 +7,29 @@
 #include "geometry_msgs/msg/point.hpp"
 #include <cmath>
 
+/**
+ * @file turtlebot3_pose_trajectory.hpp
+ * @brief Trajectory recoding for the Turtlebot
+ * 
+ * This class encapsulates the publishing and subscribing
+ * for the pose trajectory tracking. 
+ */
+
+
 class PoseTrajectory : public rclcpp::Node
 {
     public:
         PoseTrajectory();
 
     private:
-
+        
+        // Callback function for Odometry
         void odom_callback(const nav_msgs::msg::Odometry::SharedPtr p_msg);
 
+        // Takes the current point from Odometry and publishes and tracks it
         void track_position(geometry_msgs::msg::Point p_position);
 
+        // ROS publishers and subscribers
         rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
         rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr trajectory_pub_;
         visualization_msgs::msg::Marker trajectory_marker;
